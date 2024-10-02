95c5d91aa4f50d7f7014297d6f4a5cbd3b369e94
Krishna <konw1singh@gmail.com>
2024-10-02 14:30:45 +0530

    Practical Questions

--- Readme.md
+++ Readme.md
@@ -1,1 +1,27 @@
 // Practical JavaScript InterView Question
+
+// 1. How to sum of array values [1,2,3,4,5]
+
+// 2. How to flat the array [1,2,3,4,[5],6]
+
+// 3. Print Numbers 1 to 100 using For Loop
+
+// 4. Print Numbers 1 to 100 using For Loop
+
+//5. How to add value in array specific position - splice() method it takes 3 arguments (position, add/remove, value)
+// Position means - start poisition
+//0 means - Add
+//1 means - Remove
+// value
+
+//6. How to find largest number in array using sort()
+// - Smallest value
+// - Second Largest value
+
+//7. Difference between Map & Filter Functions
+//Map()
+//Filter()
+
+//8. Difference between let var and const
+//9. What is Destructuring in Javascript
+
--- script.js
+++ script.js
@@ -1,7 +1,184 @@
-// How to Get a Sum of Array
+// How to sum of array values
 // Using Reduce
 let sum = [1,2,3,4,5];
-let result = sum.reduce((a,c)=>{
+let total = sum.reduce((a,c)=>{
     return a + c;
 })
-console.log(result);
+//console.log(total);
+
+// Use flat array
+let arr = [1,2,3,4,[5],6];
+//console.log(arr.flat());
+// Output = [1,2,3,4,5,6]
+
+// Using Flat() Neseted array
+let nestedArr = [1,2,3,4,[5,6,[7,8]]];
+//console.log(nestedArr.flat(Infinity));
+// Output = [1,2,3,4,5,6,7,8]
+
+// Print Numbers 1 to 100 using For Loop
+for (let i=1; i<=100; i++){
+   // console.log(i);
+}
+// Output - 1,2,3,4,5,6,7,8,9,10,.........100
+
+// Print Numbers 1 to 100 without using For Loop
+// function count(start, end) {
+//     console.log(start); // Log the current value of start
+//     if (start < end) {
+//         count(start + 1, end); // Recursively call the function with the incremented value
+//     }
+// }
+// count(1, 100);
+
+
+// function count(start, end){
+//     console.log(start);
+//     if(start<end){
+//         count(start +1, end)
+//     }
+// }
+// count(1,100);
+
+//How to add value in array specific position - splice()
+// Add Example
+const month = ["Jan", "Feb", "March"];
+month.splice(2,0,"March");
+//console.log(month);
+//Output - [ 'Jan', 'Feb', 'March', 'March' ]
+
+// Remove Example
+//Remove Duplicate March Value
+month.splice(2,1);
+//console.log(month);
+
+//How to find largest number in array sort()
+// Smalest Value
+const smallVal = [50,30,20,10,1];
+smallVal.sort((a,b)=>a-b);
+//console.log("Smallest Value" + "", smallVal);
+//output - Smallest Value [ 1, 10, 20, 30, 50 ]
+
+//Second Largest Value
+const secondLargeVal = [50,60,70,80,90,100];
+secondLargeVal.sort((a,b)=>a-b);
+//console.log("Second Largest Value" + "", secondLargeVal[secondLargeVal.length-2]);
+//console.log("Second Largest Value" + "", secondLargeVal[secondLargeVal.length-2]);
+
+//7. Difference between Map & Filter Functions
+//Map()
+let mapFun = [1,2,3,4,5];
+// let multiply= mapFun.map((val)=>val * 2);
+// console.log(multiply);
+let multiply = mapFun.map((val)=> val *2);
+//console.log(multiply);
+
+//Filter()
+let filterFun = [1,2,3,4,5];
+// let filter = filterFun.filter((val)=> val>3);
+//console.log(filter);
+
+let filter = filterFun.filter((val)=> val>=3);
+//console.log(filter);
+
+// Filter() Method with Array of Object
+let arrObj = [
+    {
+        name : "More",
+        age : 29
+    },
+    {
+        name : "Krishna",
+        age : 30
+    },
+    {
+        name : "Ajit",
+        age : 35
+    },
+    {
+        name : "Hemant",
+        age : 40
+    },
+    {
+        name : "Deepak",
+        age : 35
+    }
+];
+let result = arrObj.filter((val)=> val.age>30);
+//console.log(result);
+
+//What is Destructuring in Javascript
+//Object Destructuring
+
+let obj = {
+    name : 'Krishna',
+    age :35,
+}
+let {name,age} = obj;
+//console.log(`My name is ${name} and age is ${age}`);
+
+// Assigining new variable
+let obj1 = {
+    name : 'Ram',
+    age :30,
+}
+let {name:newname, age:newage}=obj1;
+//console.log(`My name is ${newname} and age is ${newage}`);
+
+
+let obj2 = {
+    name : 'Ram',
+    age :30,
+}
+//let {...rest}=obj2;
+//console.log(rest);
+
+//Array Destructuring
+
+let arrD = [1,2,3,4,5]
+//let [a,b,c,d,e] = arrD;
+
+// Using Rest Operator
+let [a,...rest] = arrD;
+//console.log(a,rest);
+
+// Swap Number 
+// Old way
+x = 10;
+y = 20;
+temp = x;
+x=y;
+y= temp;
+
+//console.log(x);
+//console.log(y);
+// With Destructuring Sway number
+[y,x] = [x,y];
+console.log(x);
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
